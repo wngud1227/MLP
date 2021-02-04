9dfa61cf6f8c764a70b2b984e32ab102a8035b1a
wngud1227 <76057456+wngud1227@users.noreply.github.com>
2021-02-05 08:01:07 +0900

    Update MLP.py

--- MLP.py
+++ MLP.py
@@ -1,266 +1,234 @@
+import sys, os
+sys.path.append(os.pardir)
+from mnist import load_mnist
+
+
 import numpy as np
-from tensorflow.keras.datasets import mnist
+from collections import OrderedDict
 
-import tensorflow as tf
-gpus = tf.config.experimental.list_physical_devices('GPU')
-if gpus:
-    try:
-        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
-        tf.config.experimental.set_virtual_device_configuration(
-            gpus[0],
-            [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=7000)]
-        )
+#initialization
+def Random_initialization(n_in, n_out):
+    return np.random.randn(n_in, n_out)
 
-    except RuntimeError as e:
-        print(e)
+def Xavier_initialization(n_in, n_out):
+    weight = np.random.randn(n_in, n_out)
+    var = np.sqrt(n_in)
+    return weight / var
 
-def one_hot(x):
-    y = []
-    for i in x:
-        if i == 0:
-          y.append([1,0,0,0,0,0,0,0,0,0])
-        elif i == 1:
-          y.append([0,1,0,0,0,0,0,0,0,0])
-        elif i == 2:
-          y.append([0,0,1,0,0,0,0,0,0,0])
-        elif i == 3:
-          y.append([0,0,0,1,0,0,0,0,0,0])
-        elif i == 4:
-          y.append([0,0,0,0,1,0,0,0,0,0])
-        elif i == 5:
-          y.append([0,0,0,0,0,1,0,0,0,0])
-        elif i == 6:
-          y.append([0,0,0,0,0,0,1,0,0,0])
-        elif i == 7:
-          y.append([0,0,0,0,0,0,0,1,0,0])
-        elif i == 8:
-          y.append([0,0,0,0,0,0,0,0,1,0])
-        elif i == 9:
-          y.append([0,0,0,0,0,0,0,0,0,1])
-
-    y = np.array(y)
-    return y
-
-#matrix
-
-def matrix_sum(x, y):
-    result = []
-    for i in range(y.size):
-        result.append(x[i] + y[0][i])
-    result = np.array(result)
-    result = result.reshape(1, x.size)
-    return result
-
-
-def matrix_mul(x, y):
-    result = []
-    for i in x:
-        for j in y:
-            result.append(i * j)
-    result = np.array(result)
-    result = result.reshape(x.size, y.size)
-    return result
+def He_initialization(n_in, n_out):
+    weight = np.random.randn(n_in, n_out)
+    var = np.sqrt(n_in / 2)
+    return weight / var
 
 #activation function
 
 class sigmoid:
-    def function(x):
-        y = []
-        for i in x:
-            y.append(1 / (1 + np.exp(-i)))
-        y = np.array(y)
-        return y
+    def __init__(self):
+        self.out = None
 
-    def derivative(x):
-        y = []
-        for i in x:
-            y.append(i * (1 - i))
-        y = np.array(y)
-        return y
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
 
-class relu:
-    def function(x):
-        y = []
-        for i in x:
-            y.append(np.maximum(0, i))
-        y = np.array(y)
-        return y
+        return out
 
-    def derivative(x):
-        y = []
-        for i in x:
-            y.append((i > 0).astype(i.dtype))
-        y = np.array(y)
-        return y
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+
+        return dx
 
 class tanh:
-    def function(x):
-        y = []
-        for i in x:
-            y.append(np.tanh(i))
-        y = np.array(y)
-        return y
+    def __init__(self):
+        self.out = None
 
-    def derivative(x):
-        y = []
-        for i in x:
-            y.append(1 - (i ** 2))
-        y = np.array(y)
-        return y
+    def forward(self, x):
+        out = np.tanh(x)
+        self.out = out
+
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * (1.0 + self.out)
+        return dx
+
+class relu:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+
+        return out
+
+    def backward(self, dout):
+        dout[self.mask] = 0
+        dx = dout
+
+        return dx
 
 #softmax
 
-class softmax:
-    def function(x):
-        return np.exp(x) / np.exp(x).sum()
+def softmax(x):
+    if x.ndim == 2:
+        x = x.T
+        x = x - np.max(x, axis=0)
+        y = np.exp(x) / np.sum(np.exp(x), axis=0)
+        return y.T
 
-    def derivative(x):
-        return np.matmul(x.T, x)
+    x -= np.max(x)
+    return np.exp(x) / np.exp(x).sum()
 
 
 #loss
 
-class cross_entropy:
+def cross_entropy(x, y):
+    if x.ndim == 1:
+        x = x.reshape(1, x.size)
+        y = y.reshape(1, y.size)
+    batch_size = x.shape[0]
+    return -np.sum(y * np.log(x + 1e-7)) / batch_size
 
-    def function(x, y):
-        z = x * np.log(y)
-        return -(z.sum())
+def MSE(x, y):
+    return 0.5 * np.sum((x - y) ** 2)
 
-    def derivative(x, y):
-        return -(np.log(y))
+class Softmax:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.t = None
 
-class MSE:
-    def function(x, y):
-        return 0.5 * np.sum((y - x) ** 2)
-    def derivative(x, y):
-        return x - y
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy(self.y, self.t)
+        return self.loss
 
+    def backward(self, dout=1):
+        batch = self.t.shape[0]
+        dx = (self.y - self.t) / batch
 
-#accuracy
-def accuracy(predict, label, result):
-    temp = list(predict)
-    index = temp.index(max(temp))
-    if label[index] == 1:
-        result += 1
-    return result
+        return dx
 
+class affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+
+        return dx
+
+def accuracy(x, y):
+    accuracy = np.sum(np.argmax(x, axis=1) == np.argmax(y, axis=1)) / float(x.shape[0])
+    return accuracy
 
 class MLP:
-    def __init__(self, data, label, activation, loss, epoch, lr, layer):
-        self.data = data.reshape(60000, 784)
-        self.size = data.shape
-        self.epoch = epoch
+    def __init__(self, data, label, weight_init, activation):
+        self.data = data
         self.label = label
-        self.lr = lr
-        self.layer = 0
-        self.max_layer = layer
+        self.input_size = data.shape[1]
+        self.output_size = label.shape[1]
+        self.layer = 3
+        self.params = {}
+        self.layers = OrderedDict()
+
+        if weight_init == 'Xavier' or 'xavier':
+            weight = Xavier_initialization
+
+        elif weight_init == 'He' or 'he':
+            weight = He_initialization
+
+        else:
+            weight = Random_initialization
 
         try:
-            if activation == 'relu':
-                self.activation = relu.function
-                self.derivative = relu.derivative
-            elif activation == 'sigmoid':
-                self.activation = sigmoid.function
-                self.derivative = sigmoid.derivative
-            elif activation == 'tanh':
-                self.activation = tanh.function
-                self.derivative = tanh.derivative
+            if activation == 'relu' or 'Relu':
+                self.activation = relu
+            elif activation == 'sigmoid' or 'Sigmoid':
+                self.activation = sigmoid
+            elif activation == 'tanh' or 'Tanh':
+                self.activation = tanh
+
         except:
             print('Activation function error!')
 
-        try:
-            if loss == 'cross_entropy':
-                self.loss = cross_entropy.function
-                self.loss_derivative = cross_entropy.derivative
-            elif loss == 'MSE':
-                self.loss = MSE.function
-                self.loss_derivative = MSE.derivative
-        except:
-            print("Loss function error!")
+        self.params['W1'] = weight(self.input_size, 400)
+        self.params['b1'] = np.zeros(400)
+        self.params['W2'] = weight(400, 300)
+        self.params['b2'] = np.zeros(300)
+        self.params['W3'] = weight(300, 10)
+        self.params['b3'] = np.zeros(10)
 
-    def weight_init(self, n_b, n_f):
-        return np.random.rand(n_b, n_f)
+        self.layers['A1'] = affine(self.params['W1'], self.params['b1'])
+        self.layers['F1'] = self.activation()
+        self.layers['A2'] = affine(self.params['W2'], self.params['b2'])
+        self.layers['F2'] = self.activation()
+        self.layers['A3'] = affine(self.params['W3'], self.params['b3'])
+        self.softmax = Softmax()
 
-    def feedforward(self, weight, data, bias):
-        hidden_state = np.matmul(data, weight)
-        hidden_state = matrix_sum(hidden_state, bias)
-        self.layer += 1
-        return hidden_state
+    def predict(self, x):
+        for layer in self.layers.values():
+            x = layer.forward(x)
 
-    def backpropagation(self, weight, data, alpha, label):
-        try:
-            if self.layer == self.max_layer:
-                loss = self.loss_derivative(data, label)
-                derivative = softmax.derivative(data)
-                self.layer -= 1
-                return np.matmul(loss, derivative)
+        return x
 
-            elif self.layer > 0:
-                temp = np.matmul(alpha, weight.T)
-                derivative = self.derivative(data)
-                self.layer -= 1
-                return np.multiply(temp, derivative)
+    def loss(self, x, t):
+        y = self.predict(x)
+        return self.softmax.forward(y, t)
 
-        except:
-            print('backpropagation error!')
+    def gradient(self, x, t):
+        self.loss(x, t)
+        temp = self.softmax.backward(1)
+        layers = list(self.layers.values())
+        layers.reverse()
+        for layer in layers:
+            temp = layer.backward(temp)
+
+        grads = {}
+        grads['W1'] = self.layers['A1'].dW
+        grads['b1'] = self.layers['A1'].db
+        grads['W2'] = self.layers['A2'].dW
+        grads['b2'] = self.layers['A2'].db
+        grads['W3'] = self.layers['A3'].dW
+        grads['b3'] = self.layers['A3'].db
+
+        return grads
+
+    def train(self, batch, epoch, learning_rate):
+        for i in range(epoch):
+            batch_mask = np.random.choice(self.data.shape[0], batch)
+            x = self.data[batch_mask]
+            t = self.label[batch_mask]
+
+            grads = self.gradient(x, t)
+            for grad in grads:
+                self.params[grad] -= learning_rate * grads[grad]
+
+            train_acc = accuracy(self.predict(x), t)
+            print('epoch {} accuracy : {}'.format(i, train_acc))
 
 
-#train
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+    def test(self, test_set, test_label):
+        predict = self.predict(test_set)
+        test_acc = accuracy(predict, test_label)
+        return test_acc
 
-
-with tf.device('/device:GPU:0'):
-    y_one = one_hot(y_train)
-    MLP = MLP(x_train, y_one, 'sigmoid', 'MSE', 10, 0.001, 3)
-
-    w1 = MLP.weight_init(784, 100)
-    w2 = MLP.weight_init(100, 30)
-    w3 = MLP.weight_init(30, 10)
-
-    b1 = np.zeros((1, 100))
-    b2 = np.zeros((1, 30))
-    b3 = np.zeros((1, 10))
-
-    for j in range(MLP.epoch):
-        for i in range(len(MLP.label)):
-            h1 = MLP.feedforward(w1, MLP.data[i], b1)
-            h1 = MLP.activation(h1)
-
-            h2 = MLP.feedforward(w2, h1[0], b2)
-            h2 = MLP.activation(h2)
-
-            h3 = MLP.feedforward(w3, h2[0], b3)
-            predict = softmax.function(h3)
-
-            back3 = MLP.backpropagation(weight=None, data=predict, alpha=None, label=MLP.label[i])
-            back2 = MLP.backpropagation(weight=w3, data=h2, alpha=back3, label=MLP.label[i])
-            back1 = MLP.backpropagation(weight=w2, data=h1, alpha=back2, label=MLP.label[i])
-
-            w3 -= MLP.lr * np.matmul(h2.T, back3)
-            w2 -= MLP.lr * np.matmul(h1.T, back2)
-            w1 -= MLP.lr * matrix_mul(MLP.data[i].T, back1)
-
-            b3 -= MLP.lr * back3
-            b2 -= MLP.lr * back2
-            b1 -= MLP.lr * back1
-        print("{} epoch finished".format(j + 1))
-
-#test
-    test_label = one_hot(y_test)
-    test_accuracy = 0
-    test_data = x_test.reshape(10000, 784)
-    for i in range(len(test_data)):
-        t1 = MLP.feedforward(w1, test_data[i], b1)
-        t1 = MLP.activation(t1)
-
-        t2 = MLP.feedforward(w2, t1[0], b2)
-        t2 = MLP.activation(t2)
-
-        t3 = MLP.feedforward(w3, t2[0], b3)
-        test_predict = softmax.function(t3)
-
-        test_accuracy = accuracy(test_predict, test_label[i], test_accuracy)
-
-    test_accuracy = test_accuracy / len(test_data)
-    print(test_accuracy)
+(x_train, y_train), (x_test, y_test) = load_mnist(flatten=True, normalize=True, one_hot_label=True)
+model = MLP(data=x_train, label=y_train, activation='relu', weight_init='he')
+model.train(batch=100, epoch=10000, learning_rate=0.0005)
+test_acc = model.test(x_test, y_test)
+print(test_acc)
